980929977a3c783df8213c8532bf483895cd4892
TSTS09 <sekaletchio@gmail.com>
2025-05-30 20:15:39 +0000

    feat: Enhance USSD functionality with account management options and Firebase integration

--- functions/sample-index.js
+++ functions/sample-index.js
@@ -2,38 +2,91 @@ const functions = require('firebase-functions');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const firebase = require('firebase/app');
+const { create } = require('lodash');
+require('firebase/firestore');
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.post('/ussd', (req, res) => {
-    const { sessionId, serviceCode, phoneNumber, text } = req.body;
-    let response = '';
-    
-    if (text === '') {
-        // First request
-        response = `CON What would you like to check ? 
-        1. My Account 
-        2. My Phone Number 
-        `;
-    } else if (text === '1') {
-        // Register a new patient
-        response = `Con choose account information you want to view 
-        1. Account Number 
-        2. Account balance `;
-    } else if (text === ('2')) {
-        // Get the mobile number from Firestore Database 
-        response = `END Your phone number is ${phoneNumber}`;
-    } else if (text === '1*1') {
-        // View patient records
-        const accountNumber = '1234567890'; // This should be fetched from the database
-        response = `END Your account number is ${accountNumber}`;
-    } else if (text === '1*2') {
-        // View account balance
-        const accountBalance = 'KES 1000'; // This should be fetched from the database
-        response = `END Your account balance is ${accountBalance}`;
-    }
-    
-    res.set('Content-Type', 'text/plain');
-    res.send(response)
-})
+const firebaseConfig = {
+    apiKey: "AIzaSyB-7Gd_jCUUExp-h2DYlsEGwwnfAKzbeck",
+    authDomain: "afya-a1006.firebaseapp.com",
+    projectId: "afya-a1006",
+    storageBucket: "afya-a1006.firebasestorage.app",
+    messagingSenderId: "448798271887",
+    appId: "1:448798271887:web:c7f04b6c63e3ae799a03fa",
+    measurementId: "G-GWSPKP6TVT"
+};
+firebaseConfig.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
-exports.ussd = functions.https.onRequest(app);
+
+appInit();
+function appInit() {
+
+    app.post('/ussd', (req, res) => {
+        const { sessionId, serviceCode, phoneNumber, text } = req.body;
+        let response = '';
+        let account = '';
+
+        if (text === '') {
+            // First request
+            response = `CON What would you like to check ? 
+        1. Create Record 
+        2. Update Record
+        3. Read Record
+        4. Delete Record 
+        `;
+        } else if (text === '1') {
+            account = createRec();
+        } else if (text === ('2')) {
+            account = updateRec();
+        } else if (text === ('3')) {
+            account = readRec();
+        } else if (text === ('4')) {
+            account = deleteRec();
+        } else if (text === '1*1') {
+            // View patient records
+            const accountNumber = '1234567890'; // This should be fetched from the database
+            response = `END Your account number is ${accountNumber}`;
+        } else if (text === '1*2') {
+            // View account balance
+            const accountBalance = 'GHC 1000'; // This should be fetched from the database
+            response = `END Your account balance is ${accountBalance}`;
+        }
+
+        res.set('Content-Type', 'text/plain');
+        res.send(response)
+    })
+    exports.ussd = functions.https.onRequest(app);
+}
+
+function createRec() {
+    db.collection('afya')
+        .add({
+            name: 'John Doe',
+            age: 30,
+            condition: 'Healthy',
+            createdAt: firebase.firestore.FieldValue.serverTimestamp()
+        })
+        .then(docRef => {
+            console.log('Document written with ID: ', docRef.id);
+            return docRef.id; // Return the document ID
+        })
+        .catch(error => {
+            console.error('Error adding document: ', error);
+            throw new Error('Error creating record');
+        });
+    response = `END Record created successfully.`;
+    return response;
+}
+function updateRec() {
+
+}
+function deleteRec() {
+
+}
+function readRec() {
+
+}
