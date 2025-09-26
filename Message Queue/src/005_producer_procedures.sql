1dd0c4ce28526f5cc213a7b8b581cd29700e82bd
TSTS09 <sekaletchio@gmail.com>
2025-09-26 09:22:38 +0000

    Add initial implementation of message queue system with PostgreSQL
    - Created .gitignore, LICENSE, and README.md files.
    - Added Visual Studio solution and project files.
    - Implemented EventingConsumer class for message consumption.
    - Developed message handling procedures and triggers in SQL.
    - Created example data and Python consumer script.
    - Added necessary SQL scripts for initializing the database schema and procedures.

---
+++ Message Queue/src/005_producer_procedures.sql
@@ -0,0 +1,16 @@
+/* PUBLISH */ 
+CREATE PROCEDURE mq.publish (exchange_name text, routing_key text, body json, headers hstore) 
+LANGUAGE plpgsql 
+AS $$
+DECLARE 
+  exchange_id bigint;
+BEGIN
+  SELECT e.exchange_id INTO exchange_id FROM mq.exchange e WHERE e.exchange_name = publish.exchange_name;
+  IF exchange_id IS NULL THEN
+    RAISE WARNING 'No such exchange.';
+    RETURN;
+  END IF;
+  INSERT INTO mq.message_intake (exchange_id, routing_key, body, headers)
+    VALUES (exchange_id, routing_key, body, headers);
+END;
+$$;
